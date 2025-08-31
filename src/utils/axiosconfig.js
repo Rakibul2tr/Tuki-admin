ee9cd40958374768123b06889b9388e654417456
Rakibul2tr <rakibul2tr@gmail.com>
2025-08-31 23:31:24 +0600

    solve

--- src/utils/axiosconfig.js
+++ src/utils/axiosconfig.js
@@ -2,5 +2,6 @@
 // export const base_url = "https://api.projectlive.online/";  
 // export const base_url = "https://api.nrmou.xyz/";  
 export const base_url = "https://api.nrtuki.xyz/";  
-export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiJlMjk1MjFjZC02YzQyLTRlYTMtOWNhNC1hMTVmMjVkNTFjZGEiLCJpYXQiOjE3NTM5ODAwNzksImV4cCI6MTc1NjU3MjA3OX0.CAooiB2yDfrhFp-cXs8n1tnL85Th8RE-O7AvZ7omoaA";  
+export const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NmNhYjE1MC1iNDY3LTQ1ZjYtYjRkYi0xNTI1M2FkMjdlOWQiLCJpYXQiOjE3NTY2NjEzODMsImV4cCI6MTc1OTI1MzM4M30.RJSyeSQ4660x7cVfUiWXN4UZrgMwcUtvC7O8qGbSR9o";  
 
